19055426a3bc464c60ad32921adc64f56a07b0be
ghvp <viviennepadilla14@gmail.com>
2022-11-01 18:05:57 -0700

    Added some folders and files
    Added a few folders to the repository and added an HTML file and 2 JS files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,44 @@
+//this creates a box to pop up with the text hi when the html file is loaded
+alert("hi");
+
+//This does the same thing as the alert code above
+window.alert("hello world!");
+
+//this writes text on the actual html doc rather than having the text appear in a box
+document.write("Document write test.");
+
+//assigned text to variable A and printed the text onto the html doc using the variable
+var A = " This is a string variable test.";
+document.write(A);
+
+//assigned string to variable K and printed the text using window alert
+var K = (" This is a test.");
+window.alert(K);
+
+//used slashes to escape the quotation characters to have them displayed when the text is printed
+document.write(" She said \"maybe another time\".");
+
+//concatenated a string
+document.write(" Concatenate string test."
++" Test."
++" Test2.");
+
+//assigned a concatenated string to the variable B
+var B = (" This is a concatenated string."
++"  This string is assigned to variable B. ");
+document.write(B)
+
+//assigned multiple variables and printed only one
+var family =" Ages", dad = " 53", mom = " 53",
+son = " 18";
+document.write( son);
+
+//printed an expression
+3 + 3;
+document.write(3+3);
+
+//concatenating two variable strings together
+var Sent1 = " This is the beginning of the string";
+var Sent2 = " and this is the end of the string.";
+document.write(Sent1
+    + Sent2);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/string_assignment.js
@@ -0,0 +1,3 @@
+var K = ("This is for an assignment");
+
+window.alert(K);
